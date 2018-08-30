81715e9a1d747103b3088f7dc3f0e6f2ba8869cf
kornden <kornden@gmail.com>
2018-08-30 17:11:52 +0300

    hardcoded sitemap

--- mancpmsd/sitemaps.py
+++ mancpmsd/sitemaps.py
@@ -1,9 +1,14 @@
 from django.contrib import sitemaps
 from django.urls import reverse
+from django.contrib.sites.models import Site
 
 class StaticViewSitemap(sitemaps.Sitemap):
     priority = 0.5
     changefreq = 'weekly'
+
+    def get_urls(self, site=None, **kwargs):
+        site = Site(domain='mankivkacpmsd.us.to', name='mankivkacpmsd.us.to')
+        return super(StaticViewSitemap, self).get_urls(site=site, **kwargs)
 
     def items(self):
         return ['doctors', 'units', 'freedrugs']
